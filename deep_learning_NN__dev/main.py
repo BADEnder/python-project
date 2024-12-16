ae32fd9d8aa12b9ae1c24cb90d1c35a836e34bb8
BADEnder <s10183001@gmail.com>
2024-12-16 21:31:20 +0800

    20241216-03

--- deep_learning_NN__dev/main.py
+++ deep_learning_NN__dev/main.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import numpy as np
 import tensorflow as tf
-import tensorflow_hub as hub
+# import tensorflow_hub as hub
 from matplotlib import pyplot as plt
 
 cols = ['site', 'pno', 'category_1', 'category_2', 'category_3', 'desc_cn', 'spec', 'desc_en']
@@ -11,9 +11,8 @@ df = pd.read_csv('data.csv')
 df.columns = cols
 
 
-df['label'] = df['category_1']
 # df['label'] = df['category_1'] + '_' + df['category_2'] + '_' + df['category_3']
-
+df['label'] = df['category_1']
 
 remove_cols = ['category_1', 'category_2', 'category_3']
 x_cols = ['site', 'pno', 'desc_cn', 'spec', 'desc_en']
@@ -32,7 +31,7 @@ for i in check_set:
 df['label'] = df['label'].map(dict)
 
 
-tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=500000)
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=5000)
 pad_sequences = tf.keras.preprocessing.sequence.pad_sequences
 
 for col in x_cols:
@@ -53,34 +52,45 @@ col_desc_cn = pad_sequences(df['desc_cn'].to_numpy(), maxlen=50)
 col_spec = pad_sequences(df['spec'].to_numpy(), maxlen=50)
 col_desc_en = pad_sequences(df['desc_en'].to_numpy(), maxlen=50)
 
-X, Y = np.stack((col_site, col_pno, col_desc_cn, col_spec, col_desc_en), axis=1), df['label'].values
+# X, Y = np.stack((col_site, col_pno, col_desc_cn, col_spec, col_desc_en), axis=1), df['label'].values
+X, Y = np.stack((col_pno, col_desc_cn), axis=1), df['label'].values
+# X, Y = np.stack((col_pno, col_desc_cn, col_desc_en), axis=1), df['label'].values
 
 
 model = tf.keras.Sequential([
-      tf.keras.layers.Flatten(input_shape=(5, 50)),
-      tf.keras.layers.Dense(1024, activation='relu'),
-      tf.keras.layers.Dense(2048, activation='relu'),
+      tf.keras.layers.Flatten(input_shape=(2, 50)),
+    #   tf.keras.layers.Flatten(input_shape=(3, 50)),
+      tf.keras.layers.Dense(32, activation='relu'),
+      tf.keras.layers.Dropout(0.1),
+      tf.keras.layers.Dense(64, activation='relu'),
+      tf.keras.layers.Dropout(0.1),
+      tf.keras.layers.Dense(32, activation='relu'),
       tf.keras.layers.Dense(len(df['label'].unique()), activation='softmax')
 ])
 
 model.compile(
-            optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-            # loss=tf.compat.v1.losses.sparse_softmax_cross_entropy,
-            metrics=['accuracy']
-        )
+      optimizer=tf.keras.optimizers.Adam(0.001),
+      loss='sparse_categorical_crossentropy',
+      metrics=['accuracy']
+)
 
+print(X.shape)
+print(Y.shape)
 
+print('------\n'*3)
+
+print('SUMMARY:\n', model.summary())
+# print('BEFORE:\n', model.evaluate(X, Y))
 
 history = model.fit(
   X, Y, 
-  epochs=5, batch_size=32, 
-  validation_split=0.2,
+  epochs=10, 
+  batch_size=32, 
+  validation_split=0.1,
 #   verbose=0
 )
 
-
-model.evaluate(X, Y)
+# print('AFTER:\n', model.evaluate(X, Y))
 
 
 plt.plot(history.history['accuracy'], label='accuracy')
